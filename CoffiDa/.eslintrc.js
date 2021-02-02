21520d5c655498e5e2175844fb7c2d521149d7e2
Oreberry <aubreymonk1@gmail.com>
2021-02-02 20:22:39 +0000

    Update .eslintrc.js
    For airbnb style guide

--- CoffiDa/.eslintrc.js
+++ CoffiDa/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community'
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
+  "parser": "babel-eslint",
+  "ecmaFeatures": {
+    "classes": true
+  },
+  "rules": {
+    "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+    "linebreak-style": ["error", "windows"]
+  }
 }

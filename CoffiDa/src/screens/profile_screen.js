6bf93d76c403c10f91cf3a86d7b2f99726c1f10f
Oreberry <aubreymonk1@gmail.com>
2021-03-04 03:43:38 +0000

    Move dark theme toggle to profile screen

--- CoffiDa/src/screens/profile_screen.js
+++ CoffiDa/src/screens/profile_screen.js
@@ -1,36 +1,67 @@
 import * as React from 'react';
 import {View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {
+  Button,
+  Text,
+  TouchableRipple,
+  Switch,
+  useTheme,
+} from 'react-native-paper';
 import PropTypes from 'prop-types';
 import Logout from '../components/logout';
 import globalStyles from '../styles/global_stylesheet';
+import {Context} from '../components/context';
 
-const ProfileScreen = (props) => (
-  <View style={globalStyles.flexContainer}>
-    <View style={globalStyles.twoButtonViewOne}>
-      <Button
-        contentStyle={globalStyles.buttonContent}
-        style={globalStyles.alternativeButton}
-        mode="contained"
-        onPress={() =>
-          props.navigation.navigate('homeStackNavigator', {
-            screen: 'Update Details',
-          })
-        }>
-        <Text>Update Details</Text>{' '}
-      </Button>
+const ProfileScreen = (props) => {
+  const paperTheme = useTheme();
+  const {toggleTheme} = React.useContext(Context);
+
+  return (
+    <View style={globalStyles.flexContainer}>
+      <TouchableRipple
+        onPress={() => {
+          toggleTheme();
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+            paddingVertical: '4%',
+            paddingHorizontal: '4%',
+          }}>
+          <View style={{marginTop: '0.7%'}}>
+            <Text>Dark Theme</Text>
+          </View>
+          <View pointerEvents="none">
+            <Switch value={paperTheme.dark} />
+          </View>
+        </View>
+      </TouchableRipple>
+      <View style={globalStyles.twoButtonViewOne}>
+        <Button
+          contentStyle={globalStyles.buttonContent}
+          style={globalStyles.alternativeButton}
+          mode="contained"
+          onPress={() =>
+            props.navigation.navigate('homeStackNavigator', {
+              screen: 'Update Details',
+            })
+          }>
+          <Text>Update Details</Text>{' '}
+        </Button>
+      </View>
+      <View style={globalStyles.twoButtonViewTwo}>
+        <Button
+          contentStyle={globalStyles.buttonContent}
+          style={globalStyles.alternativeButton}
+          mode="contained"
+          onPress={() => Logout(props)}>
+          <Text>Logout</Text>{' '}
+        </Button>
+      </View>
     </View>
-    <View style={globalStyles.twoButtonViewTwo}>
-      <Button
-        contentStyle={globalStyles.buttonContent}
-        style={globalStyles.alternativeButton}
-        mode="contained"
-        onPress={() => Logout(props)}>
-        <Text>Logout</Text>{' '}
-      </Button>
-    </View>
-  </View>
-);
+  );
+};
 
 ProfileScreen.propTypes = {
   navigation: PropTypes.shape({

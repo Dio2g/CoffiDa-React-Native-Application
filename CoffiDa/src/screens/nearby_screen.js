ff19118520a848d8d0ec659057a7c3898ce71a25
Oreberry <aubreymonk1@gmail.com>
2021-03-04 19:41:07 +0000

    Add nearest locations
    Feat: Add nearest location list in nearby and enable user to click to go to the same region on map

--- CoffiDa/src/screens/nearby_screen.js
+++ CoffiDa/src/screens/nearby_screen.js
@@ -1,21 +1,32 @@
-import React, {useState, useEffect, ToastAndroid} from 'react';
+import React, {useState, useEffect, ToastAndroid, useRef} from 'react';
 import {ActivityIndicator, Text, useTheme} from 'react-native-paper';
-import {Alert, View, StyleSheet} from 'react-native';
+import {
+  Alert,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
+import {getDistance} from 'geolib';
 import globalStyles from '../styles/global_stylesheet';
 import FindLocations from '../components/find_locations';
 
 const NearbyScreen = (props) => {
+  // so paper theme colors can be used with with non paper components
+  const {colors} = useTheme();
+
+  const {navigation} = props;
+
+  const mapViewRef = useRef();
+
   const [currentLocation, setCurrentLocation] = useState({
     latitude: 0,
     longitude: 0,
   });
-
-  // so paper theme colors can be used with with non paper components
-  const {colors} = useTheme();
 
   const [listData, setListData] = useState([]);
 
@@ -46,29 +57,48 @@ const NearbyScreen = (props) => {
 
     const getLocations = async () => {
       try {
-        const parameters = {
+        const data = await FindLocations(props, {
           query: '',
           overallRating: 0,
           priceRating: 0,
           qualityRating: 0,
           clenlinessRating: 0,
           searchIn: '',
-          limit: 100,
+          limit: 50,
           offset: 0,
-        };
-        const data = await FindLocations(props, parameters);
+        });
 
-        setListData(data);
-        setIsLoading(false);
+        setListData(
+          data.sort((a, b) => {
+            const aDist = getDistance(currentLocation, {
+              lat: a.latitude,
+              lon: a.longitude,
+            });
+            const bDist = getDistance(currentLocation, {
+              latitude: b.latitude,
+              longitude: b.longitude,
+            });
+
+            return aDist - bDist;
+          }),
+        );
       } catch (e) {
         // console.error(e);
         ToastAndroid.show('Unexpected Error.', ToastAndroid.SHORT);
       }
     };
 
-    findCoordinates();
-    getLocations();
-  }, [props]);
+    const unsubscribe = navigation.addListener('focus', async () => {
+      // The screen is focused
+      setIsLoading(true);
+      findCoordinates();
+      await getLocations();
+      setIsLoading(false);
+    });
+
+    // Return the function to unsubscribe from the event so it gets removed on unmount
+    return unsubscribe;
+  }, [props, navigation, currentLocation]);
 
   if (isLoading === true) {
     return (
@@ -78,47 +108,99 @@ const NearbyScreen = (props) => {
     );
   }
   return (
-    <View style={[styles.nearbyView, {borderColor: colors.primary}]}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        tintColor="green"
-        region={{
-          latitude: currentLocation.latitude,
-          longitude: currentLocation.longitude,
-          latitudeDelta: 0.009,
-          longitudeDelta: 0.009,
-        }}>
-        <Marker
-          coordinate={currentLocation}
-          title="My Location"
-          description="Here I am"
-        />
-        {listData.map((marker) => (
+    <View
+      style={[
+        styles.nearbyView,
+        {
+          borderColor: colors.accent,
+          borderBottomColor: colors.primary,
+        },
+        {backgroundColor: colors.accent},
+      ]}>
+      <View style={styles.mapView}>
+        <MapView
+          ref={mapViewRef}
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          tintColor="green"
+          region={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.009,
+            longitudeDelta: 0.009,
+          }}>
           <Marker
-            key={marker.location_id} // Need to be unique
-            coordinate={{
-              latitude: marker.latitude,
-              longitude: marker.longitude,
-            }}
-            onPress={() => {
-              props.navigation.navigate('homeStackNavigator', {
-                screen: 'Location Info',
-                params: {locationId: marker.location_id},
-              });
-            }}>
-            <Icon
-              style={styles.markerIcon}
-              name="coffee"
-              size={40}
-              color={colors.background}
-            />
-            <Text style={{color: colors.background}}>
-              {marker.location_name}
-            </Text>
-          </Marker>
-        ))}
-      </MapView>
+            coordinate={currentLocation}
+            title="My Location"
+            description="Here I am"
+          />
+          {listData.map((marker) => (
+            <Marker
+              key={marker.location_id} // Need to be unique
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+              onPress={() => {
+                props.navigation.navigate('homeStackNavigator', {
+                  screen: 'Location Info',
+                  params: {locationId: marker.location_id},
+                });
+              }}>
+              <Icon
+                style={styles.markerIcon}
+                name="coffee"
+                size={40}
+                color={colors.background}
+              />
+              <Text style={{color: colors.background}}>
+                {marker.location_name}
+              </Text>
+            </Marker>
+          ))}
+        </MapView>
+      </View>
+      <View style={globalStyles.flexContainer}>
+        <Text style={styles.title}>Nearest Locations: </Text>
+        <FlatList
+          role="list"
+          data={listData}
+          renderItem={({item}) => {
+            const distance = getDistance(currentLocation, {
+              latitude: item.latitude,
+              longitude: item.longitude,
+            });
+            return (
+              <View role="listitem">
+                <TouchableOpacity
+                  style={[
+                    {backgroundColor: colors.primary, borderColor: colors.text},
+                    styles.touchableOpacity,
+                  ]}
+                  onPress={() => {
+                    mapViewRef.current.animateToRegion({
+                      latitude: item.latitude,
+                      longitude: item.longitude,
+                      latitudeDelta: 0.006,
+                      longitudeDelta: 0.006,
+                    });
+                  }}
+                  role="button">
+                  <Text style={styles.text}>{item.location_name}</Text>
+                  <View>
+                    {distance === 2954382 ? (
+                      <Text style={styles.text}>No location listed.</Text>
+                    ) : (
+                      <Text style={styles.text}>{distance}m away.</Text>
+                    )}
+                  </View>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.location_id.toString()}
+        />
+      </View>
     </View>
   );
 };
@@ -126,6 +208,7 @@ const NearbyScreen = (props) => {
 NearbyScreen.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
+    addListener: PropTypes.func.isRequired,
   }).isRequired,
 };
 
@@ -142,6 +225,22 @@ const styles = StyleSheet.create({
   nearbyView: {
     flex: 1,
     borderWidth: 6,
+  },
+  mapView: {
+    flex: 1.5,
+  },
+  touchableOpacity: {
+    margin: '1%',
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 17,
+    margin: '1%',
+  },
+  text: {
+    fontSize: 15,
+    margin: '1%',
   },
 });
 
